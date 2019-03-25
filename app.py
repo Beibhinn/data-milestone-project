46da1257fafa3aba0ca81563ca89163d1bc16795
Beibhinn Keenan <beibhinn.keenan@gmail.com>
2019-03-25 16:57:59 +0000

    Add page and form to edit existing recipe

--- app.py
+++ app.py
@@ -39,6 +39,12 @@ def insert_recipe():
     return redirect(url_for('get_recipes'))
 
 
+@app.route('/edit_recipe/<recipe_id>')
+def edit_recipe(recipe_id):
+    the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    return render_template('editrecipe.html', recipe=the_recipe)
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
